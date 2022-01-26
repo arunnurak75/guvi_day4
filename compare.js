bbfdb508ec96ec054d3de1b7319ae6adb0d6e8f2
arunnurak75 <arunnurak75@gmail.com>
2022-01-27 04:15:22 +0530

    compare two JSON

--- compare.js
+++ compare.js
@@ -1,4 +1,5 @@
+//compare two JSON with same properties without order
 var obj1 = { name: "Person 1", age:5 };
 var obj2 = { age:5, name: "Person 1" };
-let a=JSON.stringify(obj1) === JSON.stringify(obj2)
-console.log(a) 
+let a=JSON.stringify(obj1)===JSON.stringify(obj2)
+console.log(a)
