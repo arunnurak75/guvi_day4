72ebe621548d00085267c2d7b300390125fe13a9
arunnurak75 <arunnurak75@gmail.com>
2022-02-21 20:48:17 +0530

    task1

---
+++ task1.js
@@ -0,0 +1,50 @@
+//Task 1: Simple Programs todo for variables
+//1. Declare four variables without assigning values and print them in console
+var a;
+var b;
+var c;
+var d;
+console.log(a,b,c,d);
+
+//2.How to get value of the variable myvar as output
+var myvar= 1;
+console.log(myvar);
+
+//3. Declare variables to store your first name, last name, marital status, country and age in multiple lines
+let a={
+    first_name:"ARUN",
+    last_name:"KARUPPAIAH",
+    marital_status:"SINGLE",
+    country:"INDIA",
+    age:"25"
+    }
+    console.log(a);
+
+//4. Declare variables and assign string, boolean, undefined and null data types
+var a="task";
+var b=true;
+var c;
+var d=null;
+console.log(typeof(f));
+console.log(typeof(g));
+console.log(typeof(h));
+console.log(typeof(l));
+
+//5.Convert the string to integer
+//Convert the string to integer
+var a="1";
+console.log(parseInt(a));
+
+var b="2";
+console.log(+b);
+
+var c="3";
+console.log(Number(c));
+
+//6. Write 6 statement which provide truthy & falsey values.
+console.log(1===1);
+console.log(1===2);
+console.log(1!=2);
+console.log(1!=1);
+console.log(2>=1);
+console.log(2<=1);
